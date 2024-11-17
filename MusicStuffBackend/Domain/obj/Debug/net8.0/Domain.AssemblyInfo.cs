d8ad3d12a073c149edba092d314b421d4021d03e
BugaenkoVladislav <bugaenkovlad201@gmail.com>
2024-11-17 16:30:15 +0500

    Moved protos in domain and created AuthService

--- MusicStuffBackend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ MusicStuffBackend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc687b2a3ab8f4e5a39e7d78e488779594c8f8e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e357fde30624ba78d02eb116002df429ca0db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
